4c111d61ff7e14ce88b51e01bbf1417dc11b0573
mdcgl96 <decastrolopez.martinguillermo96@gmail.com>
2026-05-10 19:10:56 +0200

    Change fuer Render

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.get('/', (req, res) => {
     <html>
       <head><title>Test</title></head>
       <body>
-        <h1>Hello FS2026 DevOps Course!:D :D LOL</h1>
-        <p>Das wäre eine Anpassung</p>
-        <p>Noch mehr Anpassungen...</p>
+        <h1>Hello FS2026 DevOps Course auf Render!</h1>
       </body>
     </html>
   `);
